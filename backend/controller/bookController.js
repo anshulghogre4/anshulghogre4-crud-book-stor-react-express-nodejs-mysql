ff5adf5a72cc6bed42311fcec6156cba732a6f26
anshulghogre4 <anshulghogre4@gmail.com>
2023-01-11 00:38:46 +0530

    forth commit updated REST API

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -1,25 +1,107 @@
-const Book = require("../models/book")
+const { where } = require("sequelize");
+
+const Book = require("../models/book").Book
 
 exports.getAllBooks = async (req, res)=>{
-      
-        res.status(200).send("bahut der se tanga rkha tha haina");
+      try {
+        const books = await Book.findAll();
+        if (books.length>0) {
+              return  res.status(200).json({
+                        books,
+                        status:true,
+                        message: "books found"
+                }) 
+        }else return res.status(401).json("Empty Books")
+      } catch (error) {
+      return res.status(400).json({      
+                status:false,
+                message: "No books found"
+        })
+      }
 }
 
-
+/*
+        
+        title: '',
+        author: '',
+        genre: '',
+        description: '',
+        published: '',
+        publisher: ''
+*/
 exports.addABook = async (req, res)=>{
-      
-        res.status(200).send("bahut der se tanga rkha tha haina");
+      try {
+        const book = req.body;
+        const newBook = await Book.create(book)
+       return res.status(200).json({
+                status: true,
+                newBook
+        })
+      } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+                status: false,
+                message:"book not added"
+        })
+      }
+        
 }
 
 
 exports.updateABook = async (req, res)=>{
       
-        res.status(200).send("bahut der se tanga rkha tha haina");
+        try {
+                const {id,title,author, genre, description,published,publisher} = req.body;
+                const updatedBook = await Book.update({title,author, genre, description,published,publisher}, {where:{id}});
+               
+                if (updatedBook[0]>0) {
+                        return res.status(200).json({
+                                status: true,
+                                updatedBook,
+                                bookId:id,
+                                message:" book updated"  
+        
+                        }) 
+                }else {
+                        return res.status(200).json({
+                                message:" book found but book not updated"  
+                        })         
+                }
+
+              } catch (error) {
+                console.log(error)
+                return res.status(400).json({
+                        status: false,
+                        message:"book not updated or not found"
+                })
+              }
 }
 
 
 
 exports.deleteABook = async (req, res)=>{
       
-        res.status(200).send("bahut der se tanga rkha tha haina");
+        try {
+                
+                const {id} = req.params;
+               const deletedBook = await Book.destroy( {where :{id}} );
+                if (deletedBook!==0) {
+                        return res.status(200).json({
+                                status: true,
+                                message:" book deleted",
+                                 
+                        })
+                } else return res.status(400).json({
+                        status: false,
+                        message:" book not deleted or book not found"  
+                }) 
+        } catch (error) {
+                console.log(error)
+                return res.status(400).json({
+                        status: false,
+                        message:" book not deleted or book not found"  
+                }) 
+
+        }
+       
 }
--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,13 +1,32 @@
-const { json } = require("body-parser");
-const User = require("../models/user");
+
+
+const User = require("../models/user").User
 
 exports.login = async (req, res)=>{
+       
+    try {
         const {email,password} = req.body
-      const user = await User.findOne({where:{email, password}})
-      res.status(200),json({
-        user,
-        message:"user found"
-      })
+        const user = await User.findOne({where:{email, password}})
+
+           user.password =undefined
+
+        if (user) {
+            res.status(200).json({
+                user,
+                message:"user found",
+                status:true
+              })
+        } else return res.send("no user found")
+        
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            message:"user not found",
+          status:false
+          })
+    }
+   
 
 
 }
